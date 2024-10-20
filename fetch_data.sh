bd550748333338cdceeb57b4824c2a4643fadb62
Felix Haba <felixhr2000@gmail.com>
2024-10-20 02:40:54 +0000

    style improvement

--- fetch_data.sh
+++ fetch_data.sh
@@ -6,5 +6,4 @@ export tmp_dir=$(mktemp -d)
 # Fetch & Preprocess
 curl -OJ --output-dir ${tmp_dir} https://download.cms.gov/nppes/${NPPES_FILENAME}.zip
 cd ${tmp_dir} && unzip ${NPPES_FILENAME}.zip  && cd - &&
-csv2parquet "${tmp_dir}/${NPIDATA_FILENAME}.csv" "./data/inputs/${NPIDATA_FILENAME}.zstd.parquet" -s ~/tools/bedrock-data/nppes.schema --max-read-records 1 --max-row-group-size 2048 --compression=zstd 
-&& rm -rf ${tmp_dir}
+csv2parquet "${tmp_dir}/${NPIDATA_FILENAME}.csv" "./data/inputs/${NPIDATA_FILENAME}.zstd.parquet" -s ~/tools/bedrock-data/nppes.schema --max-read-records 1 --max-row-group-size 2048 --compression=zstd && rm -rf ${tmp_dir}
